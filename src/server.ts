bb36c4ad7cb6cec0e5b427be06fc285a4cd9cdde
vitorwlima <vitorwlima13@gmail.com>
2021-07-05 21:53:59 -0300

    added error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import './database'
+import { handleError } from './middlewares/handleError'
 
 const app = express()
 
 app.use(express.json())
+app.use(handleError)
 
 app.listen(3333, () => console.log('SERVER IS RUNNING'))
