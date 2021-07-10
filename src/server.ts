6b2885308e7b50c350751216a13835d5fa0d7a33
vitorwlima <vitorwlima13@gmail.com>
2021-07-10 15:02:06 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,15 @@ import 'reflect-metadata'
 import express from 'express'
 import 'express-async-errors'
 
+import cors from 'cors'
 import { handleError } from './middlewares'
 
 import './database'
 import { router } from './routes'
 
-
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 app.use(handleError)
